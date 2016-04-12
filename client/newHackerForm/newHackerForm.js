87caac7ab8faa0c7f674d1c2189b019eb3bcc1dd
Tim Storck <timstorck0@gmail.com>
2016-04-11 20:22:00 -0400

    make method for insert

--- client/newHackerForm/newHackerForm.js
+++ client/newHackerForm/newHackerForm.js
@@ -15,7 +15,7 @@ Template.newHackerForm.events({
       allegiance: allegiance
     }
 
-    Hackers.insert(newHacker);
+    Meteor.call('hackers.insert', newHacker);
     alert('added:${newHacker.legalName}(${newHacker.hackerName})');
   }
 
