a2603561a80b37886075f653f6d7fe4eb18fa860
Elmannai <welmanna@ad.bridgeport.edu>
2016-05-05 12:57:19 -0400

    Applicant profile working, List of applicant shown

--- client/newapplicantForm/newApplicantForm.js
+++ client/newapplicantForm/newApplicantForm.js
@@ -1,30 +1,67 @@
-Template.newApplicantForm.events({
-  'submit #applicantForm' : function(event, template) {
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+if(Meteor.isClient) {
 
+Template.level.helpers({
+'skillsList':function() {
+   SkillsList =[];
+   SkillsList= Session.get ('SkillsList');
+   return SkillsList
+
+}
+});
+
+Template.newApplicantForm.events({
+     'click .skills':function(){
+      event.preventDefault();
+        skills = [];
+        $( "select option:selected" ).each(function(i) {
+          skills[i]= $( this ).text();
+        });
+        skillsList=[];
+        skillsList= skills;
+        Session.set('SkillsList',skillsList);
+    } ,
+
+    'submit #applicantForm' : function(event, template) {
     event.preventDefault();
     const applicantName = template.find('#applicantName').value;
     const currentJob = template.find('#currentJob').value;
     const email = template.find('#email').value;
-    //var skills =($('input[name="skills"]:checked'));
-    //var skills = $('.skillscheckbox:checked').val();
-    var skills = []; 
-    var inputElements = document.getElementsByClassName('skillscheckbox');
-    for(var i=0; i<inputElements.length; ++i){
-    if(inputElements[i].checked){
-    skills.push(inputElements[i].value);
+    const phoneNumber = template.find('#phoneNumber').value;
 
+
+    var skills = []; 
+    var inputElementslevel =[];
+     inputElementslevel= document.getElementsByClassName('level0');
+    console.log(inputElementslevel);
+    var inputElementsskills =  Session.get ('SkillsList');
+    console.log(Session.get('SkillsList'));
+  for(i=0; i< inputElementsskills.length; i++){
+    var level = $("."+inputElementsskills[i]+":checked").val();
+    skills.push({'skill':inputElementsskills[i], 'level':level});
   }
-}
+  
     const newApplicant = {
       applicantName: applicantName,
       currentJob: currentJob,
       email: email, 
+      phoneNumber: phoneNumber,
       skills: skills,
       createdBy: Meteor.userId()
     }
     Meteor.call('applicants.insert', newApplicant);
-    alert('You just added');
-  }
+    alert('You just added your information');
+  },
 
 
 });
+}
+
+Template.newApplicantForm.onRendered(function () {
+$('.ui.dropdown')
+  .dropdown({
+    allowAdditions: true
+  });
+});
+
